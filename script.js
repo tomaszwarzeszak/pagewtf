32f7cd441de90ecfc8cfde96575aac70ab3cde16
Tomasz Warzeszak <tomekwarzeszak@gmail.com>
2020-05-08 22:53:51 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const firstName = 'Tomek';
+const age = 25;
+const lastName = 'Warzeszak';
+
+console.log(`Cześć moje imię to ${firstName} nazwisko ${lastName} mam ${age} lat. 
+Witam Cię na mojej stonie internetowej mam nadzieję, że będzie Ci tutaj dobrze. `);
+
